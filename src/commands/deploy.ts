500e1085ba6b22100c19519c371a8cb91d8cd9f1
Roman Bobrovskiy <roman@dbbsoftware.com>
2024-05-10 19:58:56 +0300

    added basic CDK and CLI setup

---
+++ src/commands/deploy.ts
@@ -0,0 +1,102 @@
+import {
+  CloudFormationClient,
+  DescribeStacksCommand,
+  CreateStackCommand,
+  UpdateStackCommand,
+  DeleteStackCommand,
+  TypeNotFoundException
+} from '@aws-sdk/client-cloudformation'
+import * as cdk from 'aws-cdk-lib'
+import { buildApp } from './build'
+import { Nextjs } from '../cdk/stacks/nextJs'
+
+export interface DeployConfig {
+  siteName: string
+  stage?: string
+  pruneBeforeDeploy?: boolean
+  aws: {
+    region?: string
+    aws_access_key_id?: string
+    aws_secret_access_key?: string
+  }
+}
+
+const checkIfStackExists = async (cf: CloudFormationClient, stackName: string) => {
+  const command = new DescribeStacksCommand({ StackName: stackName })
+
+  try {
+    await cf.send(command)
+
+    return true
+  } catch (err) {
+    if (err instanceof TypeNotFoundException) {
+      return false
+    }
+
+    throw err
+  }
+}
+
+const createStack = async (cf: CloudFormationClient, stackName: string, template: string) => {
+  const command = new CreateStackCommand({
+    StackName: stackName,
+    TemplateBody: template
+  })
+
+  return cf.send(command)
+}
+
+const updateStack = async (cf: CloudFormationClient, stackName: string, template: string) => {
+  const command = new UpdateStackCommand({
+    StackName: stackName,
+    TemplateBody: template
+  })
+
+  return cf.send(command)
+}
+
+const destroyStack = async (cf: CloudFormationClient, stackName: string) => {
+  const command = new DeleteStackCommand({
+    StackName: stackName
+  })
+
+  return cf.send(command)
+}
+
+export const deploy = async (config: DeployConfig) => {
+  const { pruneBeforeDeploy = false, siteName, stage = 'production', aws } = config
+  const accessKeyId = aws.aws_access_key_id || process.env.AWS_ACCESS_KEY_ID
+  const secretAccessKey = aws.aws_secret_access_key || process.env.AWS_SECRET_ACCESS_KEY
+
+  if (!accessKeyId || !secretAccessKey) {
+    throw new Error('AWS Credential are required.')
+  }
+
+  buildApp()
+
+  const app = new cdk.App()
+  const nextjsStack = new Nextjs(app, `${siteName}-${stage}`)
+
+  const cfTemplate = app.synth().getStackByName(nextjsStack.stackName).template
+  const cf = new CloudFormationClient({
+    region: aws.region,
+    credentials: {
+      accessKeyId,
+      secretAccessKey
+    }
+  })
+
+  const ifStackExists = await checkIfStackExists(cf, nextjsStack.stackName)
+
+  if (ifStackExists && pruneBeforeDeploy) {
+    await destroyStack(cf, nextjsStack.stackName)
+  }
+
+  if (!ifStackExists || (ifStackExists && pruneBeforeDeploy)) {
+    await createStack(cf, nextjsStack.stackName, cfTemplate)
+  }
+
+  if (ifStackExists && !pruneBeforeDeploy) {
+    await updateStack(cf, nextjsStack.stackName, cfTemplate)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,64 @@
 #!/usr/bin/env node
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
-import { buildApp } from './build'
+import { deploy } from './commands/deploy'
 
-const command = process.argv[2]
-
-if (command === 'build') {
-  buildApp()
+interface CLIOptions {
+  siteName: string
+  stage?: string
+  pruneBeforeDeploy?: boolean
+  region?: string
+  aws_access_key_id?: string
+  aws_secret_access_key?: string
 }
+
+const cli = yargs(hideBin(process.argv))
+  .scriptName('@dbbs-next')
+  .usage('$0 <command> [options]')
+  .example('$0 deploy --siteName MySite --stage staging --pruneBeforeDeployment', 'Deploys the app.')
+  .option('siteName', {
+    type: 'string',
+    requiresArg: true,
+    describe: 'The name used to created CDK stack and components.'
+  })
+  .option('stage', {
+    type: 'string',
+    describe: 'The stage of your app, defaults to production'
+  })
+  .option('pruneBeforeDeploy', {
+    type: 'boolean',
+    description: 'Clear CDK stack before deployment.',
+    default: false
+  })
+  .option('aws_access_key_id', {
+    type: 'string'
+  })
+  .option('aws_secret_access_key', {
+    type: 'string'
+  })
+  .option('region', {
+    type: 'string'
+  })
+
+cli.command<CLIOptions>(
+  'deploy',
+  'app deployment',
+  () => {},
+  async (argv) => {
+    const { siteName, pruneBeforeDeploy, stage, aws_access_key_id, aws_secret_access_key, region } = argv
+
+    await deploy({
+      siteName,
+      stage,
+      pruneBeforeDeploy,
+      aws: {
+        region,
+        aws_access_key_id,
+        aws_secret_access_key
+      }
+    })
+  }
+)
+
+cli.parse()
