b93f2b54e5d5a4b3fc18e146dc5b6fb9de0d3153
Roman Bobrovskiy <roman@dbbsoftware.com>
2024-07-02 19:26:01 +0300

    fixed assigning values to handler

--- src/cacheHandler/index.ts
+++ src/cacheHandler/index.ts
@@ -1,4 +1,4 @@
-import { Cache, NextCacheHandlerContext } from '@dbbs/next-cache-handler-core'
+import { Cache } from '@dbbs/next-cache-handler-core'
 import { S3Cache } from '@dbbs/next-cache-handler-s3'
 import getConfig from 'next/config'
 import { CacheConfig } from '../types'
@@ -6,36 +6,14 @@ import { CacheConfig } from '../types'
 const { serverRuntimeConfig } = getConfig() || {}
 const config: CacheConfig = serverRuntimeConfig?.nextServerlessCacheConfig
 
-class ServerlessCache extends Cache {
-  constructor(props: NextCacheHandlerContext) {
-    super(props)
-  }
+config.noCacheRoutes?.forEach((route) => Cache.addNoCacheMatchers(route))
+config?.cacheCookies?.forEach((cookie) => Cache.addCookie(cookie))
+config?.cacheQueries?.forEach((query) => Cache.addQuery(query))
 
-  static addNoCacheRoutes(routes: string[] = []) {
-    this.noCacheRoutes = this.noCacheRoutes.concat(routes)
-    return this
-  }
-
-  static addCacheCookies(cookies: string[] = []) {
-    this.cacheCookies = this.cacheCookies.concat(cookies)
-    return this
-  }
-
-  static addCacheQueries(queries: string[] = []) {
-    this.cacheQueries = this.cacheQueries.concat(queries)
-    return this
-  }
-
-  static addDeviceSplit(split: boolean = false) {
-    this.enableDeviceSplit = split
-    return this
-  }
+if (config?.enableDeviceSplit) {
+  Cache.addDeviceSplit()
 }
 
-ServerlessCache.addNoCacheRoutes(config?.noCacheRoutes)
-  .addCacheCookies(config?.cacheCookies)
-  .addCacheQueries(config?.cacheQueries)
-  .addDeviceSplit(config?.enableDeviceSplit)
-  .setCacheStrategy(new S3Cache(process.env.STATIC_BUCKET_NAME!))
+Cache.setCacheStrategy(new S3Cache(process.env.STATIC_BUCKET_NAME!))
 
-export default ServerlessCache
+export default Cache
