e5f9f047342e41622c3f022ecd38c015d71cd2ef
oscar <oscar@sbbsoftware.com>
2024-07-30 09:58:02 +0100

    fix lint issue

--- src/lambdas/checkExpiration.ts
+++ src/lambdas/checkExpiration.ts
@@ -1,14 +1,12 @@
 import type { CloudFrontRequestEvent, CloudFrontRequestCallback, Context } from 'aws-lambda'
 
 function checkFileIsExpired(date: string): boolean {
-
-    if (date) {
-      return new Date(date).getTime() < new Date().getTime()
-    }
-
-    return false
-  
+  if (date) {
+    return new Date(date).getTime() < new Date().getTime()
   }
+
+  return false
+}
 
 export const handler = async (
   event: CloudFrontRequestEvent,
@@ -16,17 +14,15 @@ export const handler = async (
   callback: CloudFrontRequestCallback
 ): Promise<void> => {
   const request = event.Records[0].cf.request
-  const headers = request.headers;
+  const headers = request.headers
 
   const expiresSrc = 'Expires'
-   const cacheControlSrc = 'Cache-Control'
+  const cacheControlSrc = 'Cache-Control'
 
   try {
     // Check if file is expired
     if (headers[expiresSrc.toLowerCase()] && checkFileIsExpired(headers[expiresSrc.toLowerCase()][0].value)) {
-      headers[cacheControlSrc.toLowerCase()] = [
-        { key: cacheControlSrc, value: 'no-cache' },
-      ]
+      headers[cacheControlSrc.toLowerCase()] = [{ key: cacheControlSrc, value: 'no-cache' }]
     }
     callback(null, request)
   } catch (_e) {
