6280d0b70a8bc58cdddc46eac8a263fec941281b
Roman Bobrovskiy <roman@dbbsoftware.com>
2024-08-21 14:33:59 +0300

    feat: remove dist

--- dist/commands/build.d.ts
+++
@@ -1,8 +0,0 @@
-import { type ProjectSettings } from '../common/project';
-interface BuildAppOptions {
-    outputPath: string;
-    s3BucketName: string;
-    projectSettings: ProjectSettings;
-}
-export declare const buildApp: (options: BuildAppOptions) => Promise<() => void>;
-export {};
