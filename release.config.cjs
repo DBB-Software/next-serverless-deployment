6c1ee55face75ec25722379484de41efed42ee6d
Roman Bobrovskiy <roman@dbbsoftware.com>
2024-08-21 13:37:46 +0300

    setup commits rules

--- release.config.cjs
+++ release.config.cjs
@@ -4,9 +4,38 @@
 module.exports = {
   branches: ['main', 'feat/CI-1043-sementic-release', 'sementic-release'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { type: 'feat', scope: '*', release: 'minor' },
+          { type: 'bug', scope: '*', release: 'patch' },
+          { type: 'chore', release: false },
+          { breaking: true, release: 'major' }
+        ],
+        parserOpts: {
+          headerPattern: /^\((\w+)\/([A-Z]+-\d+)\):\s(.*)$/,
+          headerCorrespondence: ['type', 'scope', 'subject']
+        }
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          headerPattern: /^\((\w+)\/([A-Z]+-\d+)\):\s(.*)$/,
+          headerCorrespondence: ['type', 'scope', 'subject']
+        }
+      }
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changeLogFile: 'CHANGELOG.md'
+      }
+    ],
     [
       '@semantic-release/git',
       {
