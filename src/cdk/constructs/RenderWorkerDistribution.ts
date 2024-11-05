32d2489a333e8c3c206bbc6fdd83e511e0d31c94
Roman Bobrovskiy <roman@dbbsoftware.com>
2024-11-05 18:35:19 +0100

    feat: updated permissions to s3 bucket

--- src/cdk/constructs/RenderWorkerDistribution.ts
+++ src/cdk/constructs/RenderWorkerDistribution.ts
@@ -155,8 +155,8 @@ export class RenderWorkerDistribution extends Construct {
      */
     instanceRole.addToPolicy(
       new iam.PolicyStatement({
-        actions: ['s3:Get*', 's3:Put*'],
-        resources: [`${staticS3Bucket.bucketArn}/*`]
+        actions: ['s3:Get*', 's3:Put*', 's3:ListBucket'],
+        resources: [staticS3Bucket.bucketArn, `${staticS3Bucket.bucketArn}/*`]
       })
     )
 
